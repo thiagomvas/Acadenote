291c18de7721fb5bfc4298a7d8d292043a744e4a
Thiago Menezes <thiagomvas@gmail.com>
2024-06-30 18:43:41 -0300

    Add project files.

---
+++ Acadenote.Web/AppTheme.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.JSInterop;
+
+namespace Acadenote.Web
+{
+    public class AppTheme
+    {
+        private readonly IJSRuntime js;
+        public AppTheme(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        private bool isDarkMode;
+
+        public bool IsDarkMode
+        {
+            get { return isDarkMode; }
+            set 
+            { 
+                isDarkMode = value;
+                js.InvokeVoidAsync("setDarkMode", value);
+                OnChange?.Invoke();
+            }
+        }
+
+        public async Task<bool> IsBrowserDarkMode() => await js.InvokeAsync<bool>("isBrowserDarkMode");
+
+        public event Action OnChange;
+
+        public async Task ListenForThemeChanges()
+        {
+            var helper = DotNetObjectReference.Create(this);
+            await js.InvokeVoidAsync("addThemeEventListener", helper);
+        }
+
+        [JSInvokable]
+        public async Task SetDarkMode(bool isDarkMode)
+        {
+            IsDarkMode = isDarkMode;
+        }
+    }
+}
---
+++ Acadenote.Web/ColorUtility.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace Acadenote.Web
+{
+    public class ColorUtility
+    {
+        private readonly AppTheme theme;
+        public ColorUtility(AppTheme theme)
+        {
+            this.theme = theme;
+            Text = new(theme);
+        }
+        public readonly TextColor Text;
+        public Dictionary<string, string> GetColors()
+        {
+            // Use reflection
+            var properties = typeof(ColorUtility).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var colors = new Dictionary<string, string>();
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(string))
+                {
+                    colors[property.Name] = (string)property.GetValue(this);
+                }
+            }
+            return colors;
+        }
+
+        // When changing these, also update index.html
+        public string Background => theme.IsDarkMode ? "#121212" : "#f5f5f5"; 
+        public string Primary => theme.IsDarkMode ? "#9c27b0" : "#7b1fa2"; 
+        public string Secondary => theme.IsDarkMode ? "#26a69a" : "#00897b"; 
+
+
+        public string Success => theme.IsDarkMode ? "#388e3c" : "#81c784";
+        public string Warning => theme.IsDarkMode ? "#f57c00" : "#ffb74d";
+        public string Error => theme.IsDarkMode ? "#d32f2f" : "#e57373";
+
+        public class TextColor
+        {
+            private readonly AppTheme theme;
+            public TextColor(AppTheme theme)
+            {
+                this.theme = theme;
+            }
+            public string Primary => theme.IsDarkMode ? "#ffffff" : "#000000";
+            public string Secondary => theme.IsDarkMode ? "#cccccc" : "#666666";
+            public string Disabled => theme.IsDarkMode ? "#757575" : "#bdbdbd";
+            public string Hint => theme.IsDarkMode ? "#9e9e9e" : "#757575";
+        }
+    }
+}
