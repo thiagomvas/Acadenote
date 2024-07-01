36c55b8d55c41c3e0f8aa28028fdffeea9c65322
Thiago Menezes <thiagomvas@gmail.com>
2024-07-01 13:10:09 -0300

    Reverse test

--- Acadenote.Server/Program.cs
+++ Acadenote.Server/Program.cs
@@ -13,11 +13,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AcadenoteDbContext>(o => o.UseSqlite("Data Source=acadenote.db"));
 builder.Services.AddScoped<INoteRepository, NoteRepository>();
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.Urls.Add("http://*:80");
-
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
